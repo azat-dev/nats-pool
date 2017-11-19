39ed54ddc7277ccd4dce621cf70101d6d710c8c8
akaumov <kaumov.a.r@gmail.com>
2017-11-19 15:41:19 +0300

    Rename NatsPool to Pool

--- natspool.go
+++ natspool.go
@@ -5,11 +5,11 @@ import (
 	"sync"
 )
 
-// NatsPool is a simple connection pool for nats.io connections. It will create a small
+// Pool is a simple connection pool for nats.io connections. It will create a small
 // pool of initial connections, and if more connections are needed they will be
 // created on demand. If a connection is Put back and the pool is full it will
 // be closed.
-type NatsPool struct {
+type Pool struct {
 	pool chan *nats.Conn
 	df   DialFunc
 
@@ -27,7 +27,7 @@ type DialFunc func(url string, options ...nats.Option) (*nats.Conn, error)
 // NewCustom is like New except you can specify a DialFunc which will be
 // used when creating new connections for the pool. The common use-case is to do
 // authentication for new connections.
-func NewPoolCustom(addr string, size int, df DialFunc) (*NatsPool, error) {
+func NewPoolCustom(addr string, size int, df DialFunc) (*Pool, error) {
 	var client *nats.Conn
 	var err error
 	pool := make([]*nats.Conn, 0, size)
@@ -42,7 +42,7 @@ func NewPoolCustom(addr string, size int, df DialFunc) (*NatsPool, error) {
 		}
 		pool = append(pool, client)
 	}
-	p := NatsPool{
+	p := Pool{
 		Addr: addr,
 		pool: make(chan *nats.Conn, len(pool)),
 		df:   df,
@@ -62,13 +62,13 @@ func NewPoolCustom(addr string, size int, df DialFunc) (*NatsPool, error) {
 // nats.Connect. The size indicates the maximum number of idle
 // connections to have waiting to be used at any given moment. If an error is
 // encountered an empty (but still usable) pool is returned alongside that error
-func New(addr string, size int) (*NatsPool, error) {
+func New(addr string, size int) (*Pool, error) {
 	return NewPoolCustom(addr, size, nats.Connect)
 }
 
 // Get retrieves an available nats connections. If there are none available it will
 // create a new one on the fly
-func (p *NatsPool) Get() (*nats.Conn, error) {
+func (p *Pool) Get() (*nats.Conn, error) {
 	select {
 	case conn := <-p.pool:
 		return conn, nil
@@ -80,7 +80,7 @@ func (p *NatsPool) Get() (*nats.Conn, error) {
 // Put returns a client back to the pool. If the pool is full the client is
 // closed instead. If the client is already closed (due to connection failure or
 // what-have-you) it will not be put back in the pool
-func (p *NatsPool) Put(conn *nats.Conn) {
+func (p *Pool) Put(conn *nats.Conn) {
 	select {
 	case p.pool <- conn:
 	default:
@@ -91,7 +91,7 @@ func (p *NatsPool) Put(conn *nats.Conn) {
 // Empty removes and calls Close() on all the connections currently in the pool.
 // Assuming there are no other connections waiting to be Put back this method
 // effectively closes and cleans up the pool.
-func (p *NatsPool) Empty() {
+func (p *Pool) Empty() {
 	var conn *nats.Conn
 	for {
 		select {
@@ -106,6 +106,6 @@ func (p *NatsPool) Empty() {
 // Avail returns the number of connections currently available to be gotten from
 // the NatsPool using Get. If the number is zero then subsequent calls to Get will
 // be creating new connections on the fly
-func (p *NatsPool) Avail() int {
+func (p *Pool) Avail() int {
 	return len(p.pool)
 }
